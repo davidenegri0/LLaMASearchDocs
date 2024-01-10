3bbc0cee8a9d0d48ad6d83db21cb9e40ca77c4b4
davidenegri0 <davidenegri0@gmail.com>
2024-01-10 10:41:43 +0000

    REST API server di ricerca

--- python_scripts/webapp_REST_API/server_REST.py
+++ python_scripts/webapp_REST_API/server_REST.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from elasticsearch import Elasticsearch
+from langchain.embeddings import LlamaCppEmbeddings
 
+# Parametri
+llama_model_path = "/home/ubuntu/llama_cpp_CPUonly/models/7B/ggml-model-q4_0.gguf"
+INDEX_NAME = "ieee_db"
+
+# Model start
+embeddings = LlamaCppEmbeddings(model_path=llama_model_path)
+
+# Elasticsearch connect to db
+elastic_client = Elasticsearch("http://localhost:9200")
+
+# REST API server start
 app = Flask(__name__)
 
-@app.route("/search/<document>")
-def search_on_elastic(document):
+@app.route("/search/<knn_num>/<document>")
+def search_on_elastic(knn_num, document):
     
-    response = str(document).replace("_", " ")
-    # ...
-    # Ciao
+    abstract = str(document).replace("_", " ")
     
-    return f"Searching this doc: {response}\n"
+    embedded_abstract = embeddings.embed_query(abstract)
+
+    res = elastic_client.search(index=INDEX_NAME, knn={"field": "embedding", "k":knn_num, "num_candidates": 50, "query_vector": embedded_abstract}, pretty=True)
+
+    # print(res)
+
+    results = res["hits"]["hits"]
+    
+    return results
